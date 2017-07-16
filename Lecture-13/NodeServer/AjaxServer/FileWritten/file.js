313acd35dcfa003e61b4cc53910f197125d0b2f5
pratyush1687 <pratyush1687@gmail.com>
2017-07-16 23:59:55 +0530

    p

--- Lecture-13/NodeServer/AjaxServer/FileWritten/file.js
+++ Lecture-13/NodeServer/AjaxServer/FileWritten/file.js
@@ -6,14 +6,14 @@ var fileSystem = require('fs');
 
 function writeFile(todo) {
 
-    fileSystem.writeFile('text.txt',JSON.stringify(todo),function(req,res) {
+    fileSystem.writeFile('text.json',JSON.stringify(todo),function(req,res) {
         console.log("File is written");
     });
 }
 
 function readFile(cb) {
 
-    fileSystem.readFile('text.txt',function(err, data) {
+    fileSystem.readFile('text.json',function(err, data) {
         if(data !== undefined) {
             cb(data.toString());
         }
--- Lecture-13/NodeServer/AjaxServer/FileWritten/server.js
+++ Lecture-13/NodeServer/AjaxServer/FileWritten/server.js
@@ -14,43 +14,36 @@ var todoList = [];
 //    console.log(JSON.parse(data));
 // });
 app.use('/', express.static('public_static'));
-
-file.getData(function(data){
-    if(data.length){
-        var obj = JSON.parse(data);
-        for(let i in obj) {
-            todoList.push(obj[i]);
-        }
-    }
-
-});
-
-app.get('/add', function(req,res) {
-
-
-
-    todoList.push({task: req.query.todo});
-    file.main(todoList);
-    res.send("Sucessfully Written");
-});
-
-app.get('/getList',function(req,res){
-   file.getData(function(data){
-       res.send(data);
-   })
-});
+//
+// file.getData(function(data){
+//     if(data.length){
+//         var obj = JSON.parse(data);
+//         todoList = obj;
+//     }
+//
+// });
+//
+// app.get('/add', function(req,res) {
+//     todoList.push({task: req.query.todo});
+//     file.main(todoList);
+//     res.send("Sucessfully Written");
+// });
+//
+// app.get('/getList',function(req,res){
+//    file.getData(function(data){
+//        res.send(data);
+//    })
+// });
 
 app.post('/upload', function(req, res) {
     if (!req.files)
         return res.status(400).send('No files were uploaded.');
 
-    let textfile = req.files.foo;
+    let textfile = req.files.file;
      console.log(textfile);
     // the uploaded file object
-    textfile.mv('./text.txt');
+    textfile.mv('./music/'+textfile.name);
     res.send("File Uploaded");
 });
 
-app.listen(5000, function() {
-    console.log("Port 5000");
-});
+app.listen(5000);
